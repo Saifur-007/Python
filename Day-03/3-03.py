547b43ca2c01202f2bc7d9ec6f124488c9a87b39
Saifur Rahman Alamin <101647817+Saifur-007@users.noreply.github.com>
2023-02-22 15:40:10 +0000

    Day-03

---
+++ Day-03/3-03.py
@@ -0,0 +1,6 @@
+# ///    not      ///
+# if the result is Truth and i use "not" then the result will become False.
+# if the result is False and i use "not" then the result will become Truth.
+
+print ( not ( 3+4 == 7 ) ) # False
+print ( not ( 85 >= 90 )) # Truth
---
+++ Day-03/3-04.py
@@ -0,0 +1,10 @@
+mark = int(input("Enter your Result : "))
+
+if mark >= 90 and mark <= 100 :
+    print (" Congratulations you Pass the exam, and you got A* ")
+elif mark >= 80 and mark <= 89 :
+    print (" Congratulations you Pass the exam, and you got A ")
+elif mark >= 60 and mark <= 60 :
+    print (" Congratulations you Pass the exam. ")
+else :
+    print ( " You Failed the Exam. ")
