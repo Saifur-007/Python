6f5230149dca381f661ea2f69f522a4deaefe1a5
Saifur Rahman Alamin <101647817+Saifur-007@users.noreply.github.com>
2023-02-22 15:41:38 +0000

    Problems-Solved

---
+++ Day-03/Problems-Solved/Problem-01.py
@@ -0,0 +1,8 @@
+# Take values of length and breadth of a rectangle from user and check if is Square or not.
+length = int(input("Enter Length : " ))
+breath = int(input("Enter Breath : "))
+
+if length == breath:
+    print( "It is a Square.")
+else:
+    print( "It is not a Square.")
---
+++ Day-03/Problems-Solved/Problem-03.py
@@ -0,0 +1,8 @@
+# Write a program using conditional Statement whether a number is even ot odd.
+
+number = int(input("Enter a number to see is it Even or Odd : "))
+
+if  number%2 == 0:
+    print("This is an Even number.")
+else:
+    print("This is an Odd number.")
