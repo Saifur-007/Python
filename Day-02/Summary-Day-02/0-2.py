923315844fe955571951bf73845541d97de920e1
Saifur Rahman Alamin <101647817+Saifur-007@users.noreply.github.com>
2023-02-25 18:34:00 +0000

    Day-02_Summary

---
+++ Day-02/Summary-Day-02/0-2.py
@@ -0,0 +1,23 @@
+# arithmetic operators:
+a = 10
+b = 4
+
+print( a+b ) # 14
+print( a-b ) # 6
+print( a*b ) # 40
+print( a/b ) # 2.5
+print( a//b ) # 2
+print( 2**3) # 2^3 = 8
+print( a%b ) # 2
+
+# comparison operators :
+print ( 5 > 2 ) # True
+print ( 5 < 3 ) # False
+print ( 5 >= 3) # True
+print ( 3 <= 3) # True
+print ( 10 == 10 ) # True
+print ( 10 == 15-5 ) # True
+print ( 10 != 15 ) # True
+print ( 10 != 10 ) # False
+
+# True and False are called the Boolean values
