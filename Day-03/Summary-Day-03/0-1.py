9ab4333bdf68dd5afb72e65abb6311aeea9e566e
Saifur Rahman Alamin <101647817+Saifur-007@users.noreply.github.com>
2023-02-25 18:34:45 +0000

    Day-03_Summary

---
+++ Day-03/Summary-Day-03/0-1.py
@@ -0,0 +1,12 @@
+# ///         and           /// 
+# if // Truth and Truth = Truth
+# if // Truth and False = False
+# if // False and False = False
+# if // False and Truth = False
+
+
+
+print ( 20-4 == 16 and 10 <= 50 ) # Truth
+print ( 10 == 10 and 50 >= 45 ) # Truth
+print ( 4+5 == 9 and 9-2 == 8) # False
+print ( 9+2 == 10 and 9 <= 2 ) # False
---
+++ Day-03/Summary-Day-03/0-2.py
@@ -0,0 +1,8 @@
+# ///         or           ///
+# if // Truth or False = Truth
+# if // False or False = False
+# if // Truth or Truth = Truth
+
+print ( 3+5 == 8 or 9-1 == 10 ) # Truth
+print ( 3 >= 4 or 4*5 == 23 ) # False
+print ( 6+6 == 12 or 3**3 == 27) # Truth
